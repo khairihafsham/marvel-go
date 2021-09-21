57d7ab652f4a3f054cd198314ac9b551c00ad4bb
Khairi Hafsham <khairi@hafsham.com>
2021-09-21 21:26:32 +0800

    Uncomment initializing script and add log

--- script/warmup.sh
+++ script/warmup.sh
@@ -6,10 +6,12 @@ env >> /etc/environment
 
 service syslog-ng start
 
-# /app/script/updateAllCharacterIds.sh
+/app/script/updateAllCharacterIds.sh
 
 cp cronjob /etc/cron.d/updatefile
 
 service cron start
+
+logger "Ready to serve API"
 
 tail -f /var/log/syslog
